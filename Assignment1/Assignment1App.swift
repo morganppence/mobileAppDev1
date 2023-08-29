45d81b745ad783080021991c44991866eb76b039
morganppence <morgan.pence@colorado.edu>
2023-08-29 11:18:11 -0600

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by Morgan Pence on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1b063a574917b45e3ed39ea1b054d780035cd310
morganppence <morgan.pence@colorado.edu>
2023-08-31 13:23:59 -0600

    pls work

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -7,20 +7,66 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+    
+                
             Text("Hello, world!")
+                .fontWeight(.bold)
+            Text("Welcome to my first Xcode")
+                .foregroundColor(.green)
+            
+            Image("IMG_1239")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                .clipShape(Circle())
+                .aspectRatio(contentMode: .fit)
+            HStack{
+                Text("Aye look at my HStack")
+                MyNameInBlue()
+            }
         }
         .padding()
+        
     }
 }
+
+struct MyNameInBlue: View{
+    var body: some View {
+        VStack{
+            Text("Morgan!")
+                .foregroundColor(.green)
+            Circle()
+                .foregroundColor(.blue)
+        }
+
+    }
+}
+
+struct CircleImage: View {
+    var body: some View {
+        Image("IMG_1239")
+            .clipShape(Circle())
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct ContentView_Dark_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.dynamicTypeSize,.xxxLarge)
+    }
+}
+

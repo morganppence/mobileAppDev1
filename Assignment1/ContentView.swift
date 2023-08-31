58963058f5b3c5a2e4f8fb22926eefb661f31db2
morganppence <morgan.pence@colorado.edu>
2023-08-31 13:23:59 -0600

    Assignment 1

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -3,24 +3,58 @@
 //  Assignment1
 //
 //  Created by Morgan Pence on 8/29/23.
-//
+// used help from Youtube Video: "Easy SwiftUI Animation Tutorial with 3 Examples" by Logan Koshenka
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack{
+            LinearGradient(colors: [.green,.white], startPoint: .topLeading, endPoint: .bottomTrailing)
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            VStack{
+                Text("Hello, world!")
+                    .fontWeight(.bold)
+                Text("Welcome to my first Xcode")
+                    .foregroundColor(.blue)
+                
+                Image("IMG_1239")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    .clipShape(Circle())
+                    .aspectRatio(contentMode: .fit)
+                
+            }
+            .padding()
+            .background(.ultraThinMaterial, in:RoundedRectangle(cornerRadius: 20, style:.continuous))
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+}
+
+struct CircleImage: View {
+    var body: some View {
+        Image("IMG_1239")
+            .clipShape(Circle())
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct ContentView_Dark_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.dynamicTypeSize,.xxxLarge)
+    }
+}
+
